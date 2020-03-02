259053607387dc13e3d482ca436a1b4e3fc21f23
2and1or3 <2and1or3@gmail.com>
2020-03-02 14:43:40 +0300

    Выбор файла

---
+++ js/avatar.js
@@ -0,0 +1,28 @@
+'use strict';
+
+(function () {
+  var userChooser = document.querySelector('.ad-form__field input[type=file]');
+  var userPreview = document.querySelector('.ad-form-header__preview img');
+
+  var bookingChooser = document.querySelector('.ad-form__upload input[type=file]');
+  var bookingPreview = document.querySelector('.ad-form__photo img');
+
+  var makePreviewer = function (fileChooser, preview) {
+    fileChooser.addEventListener('change', function () {
+      var file = fileChooser.files[0];
+
+      if (file) {
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function () {
+          preview.src = reader.result;
+        });
+
+        reader.readAsDataURL(file);
+      }
+    });
+  };
+
+  makePreviewer(userChooser, userPreview);
+  makePreviewer(bookingChooser, bookingPreview);
+})();
